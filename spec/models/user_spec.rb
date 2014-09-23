5ff08c593e946560c7e94ed381d6b0bd17db0e49
BloodyDogZ <bloodydogz@gmail.com>
2014-09-23 16:27:57 -0700

    Add userfollowing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,9 +7,6 @@ describe User do
                      password: "foobar", password_confirmation: "foobar")
   end
 
-  it { should respond_to(:microposts) }
-  it { should respond_to(:feed) }
-
   subject { @user }
 
   it { should respond_to(:name) }
@@ -24,6 +21,17 @@ describe User do
 
   it { should be_valid }
   it { should_not be_admin }
+
+  it { should respond_to(:microposts) }
+  it { should respond_to(:feed) }
+  it { should respond_to(:relationships) }
+  it { should respond_to(:followed_users) }
+  it { should respond_to(:reverse_relationships) }
+  it { should respond_to(:followers) }
+  it { should respond_to(:following?) }
+  it { should respond_to(:follow!) }
+  it { should respond_to(:unfollow!) }
+
 
   describe "with admin attribute set to 'true'" do
     before do
@@ -120,7 +128,6 @@ describe User do
   end
 
   describe "micropost associations" do
-
     before { @user.save }
     let!(:older_micropost) do
       FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
@@ -137,10 +144,21 @@ describe User do
       let(:unfollowed_post) do
         FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
       end
+      let(:followed_user) { FactoryGirl.create(:user) }
+
+      before do
+        @user.follow!(followed_user)
+        3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
+      end
 
       its(:feed) { should include(newer_micropost) }
       its(:feed) { should include(older_micropost) }
       its(:feed) { should_not include(unfollowed_post) }
+      its(:feed) do
+        followed_user.microposts.each do |micropost|
+          should include(micropost)
+        end
+      end
     end
 
     it "should destroy associated microposts" do
@@ -150,6 +168,29 @@ describe User do
       microposts.each do |micropost|
         expect(Micropost.where(id: micropost.id)).to be_empty
       end
+    end
+  end
+
+  describe "following" do
+    let(:other_user) { FactoryGirl.create(:user) }
+    before do
+      @user.save
+      @user.follow!(other_user)
+    end
+
+    it { should be_following(other_user) }
+    its(:followed_users) { should include(other_user) }
+
+    describe "followed user" do
+      subject { other_user }
+      its(:followers) { should include(@user) }
+    end
+  
+    describe "and unfollowing" do
+      before { @user.unfollow!(other_user) }
+
+      it { should_not be_following(other_user) }
+      its(:followed_users) { should_not include(other_user) }
     end
   end
 end
